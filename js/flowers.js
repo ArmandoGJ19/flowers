c6b9444a6f28d5b97568f8725c5adeb52aa8a924
armandoSannin <armandosannin@gmail.com>
2024-06-22 15:59:13 -0600

    primer commit

---
+++ js/flowers.js
@@ -0,0 +1,153 @@
+let clouds = [];
+let grass = [];
+let grassColour;
+let blue = 189;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    angleMode(RADIANS);
+    grassColour = color("#C7FD48");
+    for (var i = 0; i <= width; i += 10) {
+        grass.push(new Grass(createVector(i + random(-150, 150), height - 30 + random(-20, 20))));
+    }
+    // Agregar nubes
+    addCloud(100, 100);
+    addCloud(200, 150);
+    addCloud(300, 200);
+}
+
+function draw() {
+    background(155, 186, 255, blue);
+    drawGroundPerspective();
+    stroke(grassColour, random(50, 200));
+    for (var i = 0; i < grass.length; i++) {
+        var g = grass[i];
+        g.draw();
+    }
+    drawFlowers(7);
+    drawClouds();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    grass = [];
+    for (var i = 0; i <= width; i += 10) {
+        grass.push(new Grass(createVector(i + random(-150, 150), height - 30 + random(-20, 20))));
+    }
+}
+
+function drawGroundPerspective() {
+    fill(170, 150, 146, 240);
+    noStroke();
+    beginShape();
+    vertex(0, height - 50);
+    vertex(width, height - 50);
+    vertex(width, height - 10);
+    vertex(0, height - 10);
+    endShape(CLOSE);
+}
+
+function drawClouds() {
+    for (let cloud of clouds) {
+        makeCloud(cloud.x, cloud.y);
+        cloud.x += 0.1; // Movimiento horizontal de las nubes
+    }
+}
+
+function makeCloud(cloudx, cloudy) {
+    fill(250);
+    noStroke();
+    ellipse(cloudx, cloudy, 70, 50);
+    ellipse(cloudx + 10, cloudy + 10, 70, 50);
+    ellipse(cloudx - 20, cloudy + 10, 70, 50);
+}
+
+function drawFlower(position, stemHeight, flowerHeight, flowerColor) {
+    for (var i = 0; i < 10; i++) {
+        stroke(85, 107, 47, 20);
+        strokeWeight(3);
+        if (frameCount <= 600) {
+            line(position, height - 70, position, height - 70 - stemHeight + frameCount / 10);
+        } else {
+            line(position, height - 70, position, height - 70 - stemHeight);
+        }
+        noStroke();
+    }
+
+    push();
+    fill(flowerColor);
+    translate(position, height - 70 - stemHeight);
+    noStroke();
+    for (var j = 0; j < 10; j++) {
+        if (frameCount <= 600) {
+            ellipse(0, 10 + frameCount / 20, 10 + frameCount / 40, 20 + frameCount / 20);
+        } else {
+            ellipse(0, 40, 25, 50);
+        }
+        rotate(PI / 5);
+    }
+    pop();
+}
+
+function drawFlowers(numFlowers) {
+    let basePosition = width / 2 - (numFlowers - 1) * 100 / 2;
+    let flowerColors = [
+        [230, 190, 230, 240],
+        [235, 194, 204, 240],
+        [245, 204, 174, 240],
+        [245, 174, 154, 240],
+        [245, 174, 184, 240],
+    ];
+    for (let i = 0; i < numFlowers; i++) {
+        let position = basePosition + i * 100;
+        let stemHeight = 150;
+        let flowerColor = flowerColors[i % flowerColors.length];
+        drawFlower(position, stemHeight, 50, flowerColor);
+    }
+}
+
+function Grass(loc) {
+    this.blades = [];
+    this.am = int(random(4, 12));
+    this.loc = loc;
+    this.loc.x = this.loc.x + random(-100, 100);
+
+    for (var i = 0; i < this.am; i++) {
+        this.blades.push(new Blade(random(10, 50)));
+    }
+
+    Grass.prototype.draw = function() {
+        for (var i = 0; i < this.blades.length; i++) {
+            var blade = this.blades[i];
+            push();
+            translate(this.loc.x, this.loc.y);
+            rotate(radians(blade.angle));
+            blade.branch(blade.segments);
+            pop();
+        }
+    }
+}
+
+function Blade(segments) {
+    var num = 0;
+    this.segments = segments;
+    this.angle = random(-10, 10);
+
+    this.branch = function(len) {
+        var len = len * 0.79;
+        strokeWeight(map(len, 1, this.segments, 0.1, 3));
+        line(0, 0, 0, -len);
+        push();
+        translate(0, -len);
+        if (len > 5) {
+            rotate(radians(this.angle  + sin(len+num) ));
+            this.branch(len)
+        }
+        pop();
+        num += 0.01;
+    }
+}
+
+function addCloud(x, y) {
+    clouds.push({x: x, y: y});
+}
---
+++ js/name.js
@@ -0,0 +1,107 @@
+let font;
+let points;
+let bounds;
+let nameToDisplay = '';
+
+function preload(){
+    displayFont = loadFont('./utils/Chick Regular.ttf');
+    bodyFont = loadFont('./utils/Chick Regular.ttf');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    document.getElementById('submitName').addEventListener('click', () => {
+        nameToDisplay = document.getElementById('nameInput').value;
+        document.getElementById('modal').style.display = 'none';
+        generatePoints(nameToDisplay);
+    });
+
+    document.getElementById('openModal').addEventListener('click', () => {
+        document.getElementById('modal').style.display = 'flex';
+    });
+
+    noLoop(); // Stop draw loop until name is submitted
+}
+
+function generatePoints(name) {
+    points = displayFont.textToPoints(name, 0, 0, 200, {
+        sampleFactor: 0.5,
+    });
+
+    bounds = displayFont.textBounds(name, 0, 0, 200);
+
+    for(let i = 0; i < points.length; i++){
+        let pt = points[i];
+        pt.x = pt.x - bounds.x - bounds.w / 2;
+        pt.y = pt.y - bounds.y - bounds.h / 2;
+    }
+
+    loop(); // Start draw loop after points are generated
+}
+
+function draw() {
+    background(0);
+    if (nameToDisplay) {
+        drawText();
+        drawGrass();
+    }
+}
+
+function drawText(){
+    noStroke();
+    textFont(bodyFont);
+    textSize(16);
+    fill(150);
+    text('', width / 2 - bounds.w / 2 + 30, height / 2 + bounds.h / 2 + 20);
+}
+
+function drawGrass(){
+    translate(width / 2, height / 2);
+    let from = color(78, 112, 4, 90);
+    let to = color(193, 186, 0, 90);
+    let grassColor = lerpColor(to, from, 0.5);
+
+    for(let i = 0; i < points.length; i++){
+        stroke(grassColor);
+        let p = points[i];
+        let offsetX = 0;
+        let offsetY = 0;
+        n = noise(i * 10);
+        offsetX += n * 20 - 5;
+        offsetY += noise(i * 10) * 20;
+        n = noise(i * 0.01 + millis() * -0.001);
+        offsetX += n * 10;
+        line(p.x, p.y, p.x + offsetX, p.y - 30 + offsetY);
+
+        firstPoint = points[0].x;
+        lastPoint = points[points.length-1].x;
+        let padding = 200;
+        let mappedVal = map(mouseX, 0, width, firstPoint - padding, lastPoint + padding, true);
+
+        if(mappedVal >= p.x){
+            drawFlowers(p.x + offsetX, p.y - 30 + offsetY);
+        }
+    }
+}
+
+function drawFlowers(pointX, pointY){
+    push();
+    translate(pointX, pointY);
+    stroke(0, 0, 0, 30);
+    strokeWeight(1);
+    let petalAmount = 10;
+    let pointAngle = 360 / petalAmount;
+    let radius = 0;
+    for (angle = 0; angle < 360; angle += pointAngle){
+        stroke(color(127, 134, 170, 90));
+        x = cos(radians(angle)) * 5;
+        y = sin(radians(angle)) * 5;
+        line(radius, radius, x + radius + noise(angle) * 2, y + radius + noise(angle) * 2);
+    }
+    pop();
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
